ce032d4aee90258a62da10a9a8abea5cbd49cb4c
Ezra Machoka <ezra.machoka@student.moringaschool.com>
2023-09-08 10:21:10 +0300

    completed assignment

---
+++ lib/migrations/versions/c14d9afd2824_rename_column_in_travis_model.py
@@ -0,0 +1,24 @@
+"""Rename column in travis model
+
+Revision ID: c14d9afd2824
+Revises: 791279dd0760
+Create Date: 2023-09-08 10:16:49.566836
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c14d9afd2824'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
